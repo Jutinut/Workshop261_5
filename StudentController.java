44801ce4873c0adde6645ebf1ab2a5a983ecd850
Jutinut Ratanamongkonkul-6609650228 <145243765+Jutinut@users.noreply.github.com>
2024-11-07 13:45:39 +0700

    Update StudentController.java

--- StudentController.java
+++ StudentController.java
@@ -1,10 +1,16 @@
 package com.example.crud;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/student")
+
 public class StudentController {
 	@Autowired
 	private final StudentRepository studentRepository;
