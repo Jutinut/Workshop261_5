05607de496d5bd29495a83199ffbf5878e2870fc
Jutinut Ratanamongkonkul-6609650228 <145243765+Jutinut@users.noreply.github.com>
2024-11-07 11:26:02 +0700

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud;
-import com.example.crud.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-
-}
